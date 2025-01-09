bb40630a8e16d88365cc9462f8b7c99cbbee6251
blaisep <blaise@gmail.com>
2025-01-09 15:04:29 -0500

    Add a test for the upload

--- private_facts/test/hello_world_test.py
+++ private_facts/test/hello_world_test.py
@@ -1,4 +1,5 @@
 import subprocess
+from hello_world import upload_string
 
 def test_hello_world():
     expected = "The contents of the test string are: Hello, world!"
@@ -7,3 +8,8 @@ def test_hello_world():
     )
     output = process.stdout.rstrip()
     assert output == expected
+
+def test_upload_string():
+    expected = "http://127.0.0.1:3456/uri/URI%3ADIR2%3Adjrdkfawoqihigoett4g6auz6a%3Ajx5mplfpwexnoqff7y5e4zjus4lidm76dcuarpct7cckorh2dpgq/"
+    result = upload_string()
+    assert result == expected
