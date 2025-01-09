cf33a2b4fd823de7c8c3244165348efc6c12aa95
blaisep <blaise@gmail.com>
2025-01-09 15:05:11 -0500

    add a upload method
    Co-authored-by: Jeff Jacobson <jeffjacobsonhimself@gmail.com>

--- private_facts/src/hello_world/hello_world.py
+++ private_facts/src/hello_world/hello_world.py
@@ -18,8 +18,11 @@
 """
 TEST_STRING = "Hello, world!"
 
-def main():
-    print(f"The contents of the test string are: {TEST_STRING}")
+def upload_string(fURL="http://127.0.0.1:3456/uri/URI%3ADIR2%3Adjrdkfawoqihigoett4g6auz6a%3Ajx5mplfpwexnoqff7y5e4zjus4lidm76dcuarpct7cckorh2dpgq/"):
+    return fURL
+
+def main(test_string=TEST_STRING):
+    print(f"The contents of the test string are: {test_string}")
 
 if __name__ == "__main__":
     main()
