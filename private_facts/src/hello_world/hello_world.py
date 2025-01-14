5dd422273b4be56128afc864885f7fcdebc34fa0
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-01-14 00:07:18 -0500

    Modify upload_string to upload sample string to tahoe

--- private_facts/src/hello_world/hello_world.py
+++ private_facts/src/hello_world/hello_world.py
@@ -23,22 +23,32 @@ BASE_URL="http://127.0.0.1:3456/uri/"
 
 http = urllib3.PoolManager()
 
-def get_string():
+def get_string(uri=""):
     """
     For now, set uri to the fURL of some file you've uploaded 
     """
     # Two sample URIs:
     # uri = "URI:LIT:k5ugc5dfozsxeiijbi"
     # uri = "URI:CHK:k5a6fm7527ayjcrhblng3k6lpq:7vau4zhhuun52uibdrs6bfqhvcjwcnldt7rwm3jds3v2zthtlnhq:1:1:152225"
+    uri = upload_string().data.decode("utf-8")
+    print(uri)
     resp = http.request(
     "GET",
     BASE_URL + uri
 )
+    return resp.data.decode("utf-8")
+
+
+def upload_string():
+    resp = http.request(
+    "PUT",
+    BASE_URL,
+    "Hello, world!"
+)
+    print(resp.status)
+    print(resp.data)
+
     return resp
-
-
-def upload_string(fURL="http://127.0.0.1:3456/uri/URI%3ADIR2%3Adjrdkfawoqihigoett4g6auz6a%3Ajx5mplfpwexnoqff7y5e4zjus4lidm76dcuarpct7cckorh2dpgq/"):
-    return fURL
 
 
 def main(test_string=TEST_STRING):
@@ -46,4 +56,4 @@ def main(test_string=TEST_STRING):
 
 
 if __name__ == "__main__":
-    main()
+    print(get_string())
