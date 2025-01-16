18a2078e4392cfe6c76c1c7d58ee10f1e7b65b04
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-01-15 23:43:34 -0500

    Add fake_data dict, FakeTahoe class, and new upload_string test

--- private_facts/test/hello_world_test.py
+++ private_facts/test/hello_world_test.py
@@ -3,6 +3,14 @@ import pytest
 import subprocess
 from hello_world import upload_string
 
+fake_data = {
+    'string': 'URI:CHK:3w4jfu3rtqobwjj4lhjbuclnu4:xqpceo5hvyjyb6mywufynhuhovlyhew2p7iswf77c5txlbpnljxa:1:1:7'
+}
+
+class FakeTahoe():
+    def upload(self, data_key):
+        return fake_data.get(data_key)
+
 
 @pytest.mark.skip(reason="Code has changed.")
 def test_entrypoint():
@@ -14,7 +22,13 @@ def test_entrypoint():
     assert output == expected
 
 @pytest.mark.skip(reason="Code has changed.")
-def test_upload_string():
+def test_upload_string_deprecated():
     expected = "http://127.0.0.1:3456/uri/URI%3ADIR2%3Adjrdkfawoqihigoett4g6auz6a%3Ajx5mplfpwexnoqff7y5e4zjus4lidm76dcuarpct7cckorh2dpgq/"
     result = upload_string()
+    assert result == expected
+
+def test_upload_string():
+    fake_tahoe = FakeTahoe()
+    result = fake_tahoe.upload('string')
+    expected = fake_data.get('string')
     assert result == expected
