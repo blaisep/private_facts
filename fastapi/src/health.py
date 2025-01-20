5c65522584301b6c5c0b2dd6cf22aa5a260e8f3d
Blaise Pabon <blaise@gmail.com>
2025-01-20 08:20:10 +0000

    Create scaffold fastapi project (#25)
    Inspired by Fred Cox https://github.com/sailorfred/python-meetup-2024-07-23

---
+++ fastapi/src/health.py
@@ -0,0 +1,8 @@
+"""Health data"""
+
+from enum import StrEnum
+
+HealthData = "Products_per_hour", ("0"
+                                           ""),
+                     float(BodyTemp,
+                           )
