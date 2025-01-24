1f38772293342bde5103f5132cc2da9bdd89c544
blaisep <blaise@gmail.com>
2025-01-24 12:36:42 -0500

    remove crufty model file

--- fastapi/src/health.py
+++
@@ -1,8 +0,0 @@
-"""Health data"""
-
-from enum import StrEnum
-
-HealthData = "Products_per_hour", ("0"
-                                           ""),
-                     float(BodyTemp,
-                           )
