a31a1256e2aea9ea3a79ccc11640fe10d5c7cc52
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-03-07 17:39:12 -0500

    Define mock_http and client fixtures

--- private_facts/test/tahoe_client_test.py
+++ private_facts/test/tahoe_client_test.py
@@ -1,4 +1,14 @@
+import pytest
+from unittest.mock import MagicMock
 from hello.tahoe_client import TahoeClient
+
+@pytest.fixture
+def mock_http():
+    return MagicMock()
+
+@pytest.fixture
+def client(mock_http):
+    return TahoeClient(base_url="http://127.0.0.1:3456", http=mock_http)
 
 # Client creation tests
 def test_create_client():
