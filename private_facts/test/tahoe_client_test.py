29043aba3f5f262e3c618d88f283d8562bb893bf
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-03-07 17:20:18 -0500

    Create tahoe_client_test module (WIP)

---
+++ private_facts/test/tahoe_client_test.py
@@ -0,0 +1,19 @@
+from hello.tahoe_client import TahoeClient
+
+# Client creation tests
+def test_create_client():
+    pass
+
+def test_create_client_no_url():
+    pass
+
+def test_create_client_no_http():
+    pass
+
+# Upload data tests
+
+# Retrieve data tests
+
+# Make dir tests
+
+# Get welcome tests
