3b10606894eb6ffc41ee04789d5c1f8a4ef6fd95
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-03-07 17:46:17 -0500

    Add create_client_happy test

--- private_facts/test/tahoe_client_test.py
+++ private_facts/test/tahoe_client_test.py
@@ -2,20 +2,24 @@ import pytest
 from unittest.mock import MagicMock
 from hello.tahoe_client import TahoeClient
 
+BASE_URL="http://127.0.0.1:3456"
+
 @pytest.fixture
 def mock_http():
     return MagicMock()
 
 @pytest.fixture
 def client(mock_http):
-    return TahoeClient(base_url="http://127.0.0.1:3456", http=mock_http)
+    return TahoeClient(base_url=BASE_URL, http=mock_http)
 
 # Client creation tests
-def test_create_client():
-    pass
+def test_create_client_happy(client, mock_http):
+    assert client.base_url == BASE_URL
+    assert client.http is mock_http
+    
 
-def test_create_client_no_url():
-    pass
+def test_create_client_no_url(mock_http):
+    # client = TahoeClient(mock_http)
 
 def test_create_client_no_http():
     pass
