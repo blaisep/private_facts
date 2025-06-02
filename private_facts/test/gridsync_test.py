7f12c9a7609c55ff8dc294f098205e50deb93127
jwjacobson <116485484+jwjacobson@users.noreply.github.com>
2025-06-02 13:38:31 -0400

    Gridsync integration (#45)
    * Add most gridsync fixtures to conftest.py
    
    * Add Gridsync tahoe integration tests
    
    * Comment out zkapauthorizer and skip test_no_html_in_server_error test

--- private_facts/test/gridsync_test.py
+++ private_facts/test/gridsync_test.py
@@ -1,6 +1,7 @@
 # something like uv run python private_facts/src/gridsync/tahoe.py to make sure that all the dependencies are co
 import pytest
 
+@pytest.mark.skip(reason="This just exists to make sure we can instantiate the tahoe_server fixture")
 def test_tahoe_server_fixture(tahoe_server):
     test_server = tahoe_server
     assert True
